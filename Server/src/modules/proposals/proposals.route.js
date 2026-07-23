af8aedeb8a7fa74bbcd61694c1b86ea4877c8052
SatyamRaj-WebDeveloper <151543119+SatyamRaj-WebDeveloper@users.noreply.github.com>
2026-07-23 12:50:30 +0530

    Add route to get proposals by distributor ID

--- Server/src/modules/proposals/proposals.route.js
+++ Server/src/modules/proposals/proposals.route.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
 
+// 🟢 Added getProposalsByDistributorId to the destructured imports
 const {
   createProposal,
   getAllProposals,
-  updateProposalStatus
+  updateProposalStatus,
+  getProposalsByDistributorId
 } = require('./proposal.controller');
 
 const { authenticate, authenticateDistributor } = require('../../middlewares/auth.middleware');
@@ -15,8 +17,11 @@ const checkAdminManagerHR = (req, res, next) => {
   return require('../../utils/response').fail(res, 403, 'FORBIDDEN', 'Access denied. Unauthorized role.');
 };
 
+// Distributor Routes
 router.post('/proposals', authenticateDistributor, createProposal);
 router.get('/proposals/distributor/:distributorId', authenticateDistributor, getProposalsByDistributorId);
+
+// Admin / Staff Management Routes
 router.get('/proposals/active', authenticate, checkAdminManagerHR, getAllProposals);
 router.patch('/proposals/:id/status', authenticate, checkAdminManagerHR, updateProposalStatus);
 
