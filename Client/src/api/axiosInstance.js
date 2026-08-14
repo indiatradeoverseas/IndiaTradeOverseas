6e9068ed0652882dbb81387b04badd2e773491e8
manjeet <manjeetmaurya7785@gmail.com>
2026-08-14 11:30:50 +0530

    Working on HR Portal

--- Client/src/api/axiosInstance.js
+++ Client/src/api/axiosInstance.js
@@ -1,6 +1,8 @@
 import axios from 'axios';
 
-const API_BASE_URL = 'https://indiatradeoverseas-1.onrender.com/api';
+// const API_BASE_URL = 'https://indiatradeoverseas-1.onrender.com/api';
+const API_BASE_URL = 'https://indiatradeoverseas-ito.onrender.com/api';
+
 
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
